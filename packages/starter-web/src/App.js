fe341418de1186d1326155995a63bab87d8ee8de
Bill Johnson <wrjstuff@gmail.com>
2017-09-16 17:27:17 -0400

    Added header.

--- packages/starter-web/src/App.js
+++ packages/starter-web/src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import logo from './resources/logo.svg';
-import './App.css';
+import { Provider } from 'rebass';
 
+import logo from './resources/logo.svg';
+import Header from "./components/Header";
 import Routes from "./Routes";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
+      <Provider>
+        <Header />
         <Routes />
-      </div>
+      </Provider>
     );
   }
 }
---
+++ packages/starter-web/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Toolbar } from "rebass";
+
+const Header = () => (
+    <Toolbar>
+        Megazord
+    </Toolbar>
+)
+
+export default Header;

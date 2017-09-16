2fea892b692fc3501d836ad31b0ce60d09bf150e
Bill Johnson <wrjstuff@gmail.com>
2017-09-16 15:56:47 -0400

    Reorganized again lol.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './resources/logo.svg';
-import './index.css';
+import './App.css';
+
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Home />
       </div>
     );
   }
